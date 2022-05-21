bc93221022728ecd8a94e34411756294b33fd170
Hiroaki Yutani <yutani.ini@gmail.com>
2022-05-21 19:07:44 +0900

    Update

--- thumbnail.R
+++ thumbnail.R
@@ -5,11 +5,11 @@ f <- systemfonts::system_fonts() |>
   dplyr::filter(family == "Iosevka", style == "Heavy") |>
   dplyr::pull(path)
 
-d_pkg <- string2path::string2fill("tidyr", f[1], tolerance = 0.01) |>
+d_pkg <- string2path::string2fill("webshot2", f[1], tolerance = 0.01) |>
   dplyr::mutate(
-    x = x * 1.1 + 0.5,
-    y = y * 1.1 + 0.2,
-    fill = as.integer(triangle_id + 50 * runif(dplyr::n()))
+    x = x * 1.0 - 0.1,
+    y = y * 1.0 + 0.2,
+    fill = as.integer(10 * x + 3 * y  + 1 * triangle_id + 5 * runif(dplyr::n()))
   ) |>
   dplyr::group_by(triangle_id) |>
   dplyr::mutate(
@@ -17,10 +17,10 @@ d_pkg <- string2path::string2fill("tidyr", f[1], tolerance = 0.01) |>
   ) |>
   dplyr::ungroup()
 
-d_ver <- string2path::string2fill("1.2.0", f[1], tolerance = 0.01) |>
+d_ver <- string2path::string2fill("0.1.0", f[1], tolerance = 0.01) |>
   dplyr::mutate(
-    x = x * 0.8 + 1.5,
-    y = y * 0.8 - 0.4,
+    x = x * 0.6 + 1.5,
+    y = y * 0.6 - 0.2,
     fill = as.integer(triangle_id + 60 * runif(dplyr::n()))
   ) |>
   dplyr::group_by(triangle_id) |>
@@ -55,9 +55,9 @@ p <- ggplot(mapping = aes(x, y)) +
   geom_text(data = d, aes(label = text, angle = angle), size = 6 * 4, colour = alpha("white", 0.4), family = "Iosevka") +
   geom_polygon(data = d_pkg, aes(group = triangle_id, fill = fill)) +
   geom_polygon(data = d_ver, aes(group = triangle_id, fill = fill)) +
-  annotate("text", x = 0.05, y = -0.65, label = "2022/5/17 22:00~", hjust = 0,
+  annotate("text", x = 0.05, y = -0.65, label = "2022/5/24 22:00~", hjust = 0,
            family = "Iosevka SS08", fontface = "bold", size = 6.7 * 4, colour = alpha("black", 0.87)) +
-  scale_fill_viridis_c(option = "B", guide = "none") +
+  scale_fill_viridis_c(option = "A", guide = "none") +
   coord_equal(
     xlim = c(0,  3),
     ylim = c(-1.3, 1.7) * 9 / 16
