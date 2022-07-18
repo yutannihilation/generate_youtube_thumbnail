6fceb6b8850ccba868f9be7ba9db7291923a77be
Hiroaki Yutani <yutani.ini@gmail.com>
2022-07-18 23:32:35 +0900

    Update

--- thumbnail.R
+++ thumbnail.R
@@ -1,23 +1,19 @@
 library(ggplot2)
 
-theta1 <- pi * -1 / 70
-theta2 <- pi * -1 / 26
+theta1 <- pi * -3 / 170
+theta2 <- pi * 1 / 88
 
 set.seed(15)
 
-pkg_name <- "plumber"
-pkg_ver <- "1.2.0"
-start_time <- "2022/7/12 22:00~"
+pkg_name <- "tidymodels"
+pkg_ver <- "1.0.0"
+start_time <- "2022/7/19 22:00~"
 
-f <- systemfonts::system_fonts() |>
-  dplyr::filter(family == "Iosevka", style == "Heavy") |>
-  dplyr::pull(path)
-
-d_pkg <- string2path::string2fill(pkg_name, f[1], tolerance = 0.3) |>
+d_pkg <- string2path::string2fill(pkg_name, "Noto Sans JP", font_weight = "black", tolerance = 0.01) |>
   dplyr::mutate(
     tibble::tibble(
-      x = x * 1.00 + 0.13 + 0.35 * (y - mean(y)),
-      y = y * 0.95 + 0.33
+      x = x * 0.80 - 0.05 + 0.15 * (y - mean(y)),
+      y = y * 0.77 + 0.33
     ),
     # 回転
     x = x * cos(theta1) - y * sin(theta1),
@@ -30,10 +26,10 @@ d_pkg <- string2path::string2fill(pkg_name, f[1], tolerance = 0.3) |>
   ) |>
   dplyr::ungroup()
 
-d_ver <- string2path::string2fill(pkg_ver, f[1], tolerance = 0.01) |>
+d_ver <- string2path::string2fill(pkg_ver, "Noto Sans JP", font_weight = "black", tolerance = 0.01) |>
   dplyr::mutate(
-    x = x * 0.6 + 0.6 + 0.05 * (y - mean(y)),
-    y = y * 0.6 - 0.13,
+    x = x * 0.7 + 1.1 + 0.05 * (y - mean(y)),
+    y = y * 0.7 - 0.33,
     # 回転
     x = x * cos(theta2) - y * sin(theta2),
     y = x * sin(theta2) + y * cos(theta2),
@@ -67,13 +63,18 @@ d <- data.frame(
   angle = runif(n, -20, 60)
 )
 
-p <- ggplot(mapping = aes(x, y)) +
-  geom_text(data = d, aes(label = text, angle = angle), size = 6 * 4, colour = alpha("white", 0.4), family = "Iosevka") +
-  geom_polygon(data = d_pkg, aes(x + 0.01, y - 0.03, group = triangle_id), fill = alpha("#0079dd", 0.3), colour = "transparent") +
-  geom_polygon(data = d_ver, aes(x + 0.01, y - 0.03, group = triangle_id), fill = alpha("#0079dd", 0.3), colour = "transparent") +
-  geom_polygon(data = d_pkg, aes(group = triangle_id, fill = fill), colour = alpha("white", 0.3), linewidth = 0.45) +
-  geom_polygon(data = d_ver, aes(group = triangle_id, fill = fill), colour = alpha("white", 0.3), linewidth = 0.45) +
-  scale_fill_viridis_c(option = "F", guide = "none", end = 0.85) +
+p <- ggplot(mapping = aes(x, y, group = triangle_id)) +
+  geom_text(data = d, aes(label = text, angle = angle, group = NULL),
+            size = 6 * 4, colour = alpha("white", 0.4), family = "Iosevka") +
+  geom_polygon(data = d_pkg, aes(x + 0.01, y - 0.03), fill = alpha("#0079dd", 0.3), colour = "transparent") +
+  geom_polygon(data = d_ver, aes(x + 0.01, y - 0.03), fill = alpha("#0079dd", 0.3), colour = "transparent") +
+  geom_polygon(data = d_pkg, aes(fill = fill), colour = "transparent") +
+  geom_polygon(data = d_ver, aes(fill = fill), colour = "transparent") +
+  geom_path(data = d_pkg, colour = alpha("white", 0.3), linewidth = 0.75) +
+  geom_path(data = d_ver, colour = alpha("white", 0.3), linewidth = 0.75) +
+  geom_point(data = d_pkg, colour = alpha("white", 0.3), size = 2.4) +
+  geom_point(data = d_ver, colour = alpha("white", 0.3), size = 2.4) +
+  scale_fill_viridis_c(option = "C", guide = "none", end = 0.9, direction = -1) +
   theme_void() +
   theme(plot.background = element_rect(fill = "#C5C5C5"))
 
